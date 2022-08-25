28e701b3525700c20ba432b01c3aee7e19b5dd3f
adampodkdev@gmail.com <adampodkdev@gmail.com>
2022-08-25 21:09:41 +0200

    Initial

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+import classes from "./app.module.scss";
+
+export function App() {
+  return <div class={classes.test}>Hello</div>;
+}
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+    },
+  },
+});
